4437d5a455245e1841e95a91d24fa00f3336b54a
Danik <daniknewgarden@gmail.com>
2020-09-11 19:47:51 +0300

    Aded first generation of API hook and weather UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.scss";
 import Title from "./components/Title/Title";
 import { usePosition } from "./hooks/usePosition";
+import { GetApiData } from "./hooks/getApiData";
 
 function App() {
   const position = usePosition();
+  const getData = GetApiData(position.latitude, position.longitude);
+
+  const [weather, setWeather] = useState(null);
+  const [data, setData] = useState(getData);
+
+  useEffect(() => {
+    setWeather(getData.current);
+
+    // console.log(weather);
+  }, [getData, position, weather]);
 
   return (
     <div className="App">
-      <Title text={`${position.latitude}, ${position.longitude}`} />
+      <Title text={`${position.longitude}`} />
+
+      {weather && <Title text={`${weather.temp_c}°`} />}
     </div>
   );
 }
--- src/hooks/usePosition.js
+++ src/hooks/usePosition.js
@@ -1,22 +1,28 @@
 import { useState, useEffect } from "react";
 
 export const usePosition = () => {
+  //Object with latitude, longitude, error
   const [position, setPosition] = useState({});
+  //Error
   const [error, setError] = useState(null);
 
-  const onChange = ({ coords }) => {
+  //If permission granted
+  const onSuccess = ({ coords }) => {
     setPosition({
       latitude: coords.latitude,
       longitude: coords.longitude,
     });
   };
+
+  //If permission denied or another error
   const onError = (error) => {
     setError(error.message);
   };
 
+  //Location request
   useEffect(() => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(onChange, onError);
+      navigator.geolocation.getCurrentPosition(onSuccess, onError);
     } else {
       console.log("Geolocation is not supported by this browser.");
     }
