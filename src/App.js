4b848c5a03fc1cc460baaf5c01ecb8811d57958a
Danik <daniknewgarden@gmail.com>
2020-09-17 22:55:36 +0300

    Aded useFetch hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.scss";
 import Title from "./components/Title/Title";
+import { useFetch } from "./hooks/useFetch";
 import { usePosition } from "./hooks/usePosition";
 
 function App() {
   const { location, error } = usePosition();
+
+  //Data from API
+  const [dataFromAPI, setDataFromAPI] = useState(null);
+
+  //get data from API
+  const data = useFetch(
+    `https://api.weatherapi.com/v1/current.json?key=4211749ff95543348f4191634202508&q=London`
+  );
+
+  useEffect(() => {
+    if (data) {
+      setDataFromAPI(data);
+      console.log(data);
+    }
+  }, []);
 
   return (
     <div className="App">
@@ -12,6 +28,8 @@ function App() {
         <Title text={`${location.longitude}, ${location.latitude}`} />
       )}
       {error && <Title text={`${error}`} />}
+
+      {dataFromAPI && <Title text={`data from API sucsess`} />}
     </div>
   );
 }
