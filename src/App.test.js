0e4a92a157a390a2ec718ccb776291f9ebefba09
Danik <daniknewgarden@gmail.com>
2020-09-24 21:57:03 +0300

    Aded components tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import Title from "./components/Title/Title";
+import { Subtitle } from "./components/Subtitle/Subtitle";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("Layout Title component test", () => {
+  render(<Title />);
+  screen.debug();
+});
+
+test("Layout Subtitle component test", () => {
+  render(<Subtitle />);
+  screen.debug();
+});
+
+test("Layout App component test", () => {
+  render(<App />);
+  screen.debug();
 });
